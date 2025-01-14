fb85f3753ce57aca24bf51509ad20ab5248114a8
Your Name <you@example.com>
2025-01-14 17:20:17 +0530

    changes

--- app1/views.py
+++ app1/views.py
@@ -3,16 +3,30 @@ from .models import Transaction, Budget
 from django.db.models import Sum
 from .forms import TransactonForm
 
-def dashboard(request):
+def get_data(request):
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
     total_income = transactions.filter(type='income').aggregate(Sum('amount'))['amount__sum'] or 0
+    
     total_expense = transactions.filter(type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
-    return render(request, 'index.html', {
+    total_amt = Budget.objects.first()
+    return {
         'transactions': transactions,
         'total_income': total_income,
         'total_expense': total_expense,
         'balance': total_income - total_expense,
-    })
+        'total_amount': total_amt.total_balance if total_amt else 0,
+    }
+
+def dashboard(request):
+    # transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+    # total_income = transactions.filter(type='income').aggregate(Sum('amount'))['amount__sum'] or 0
+    
+    # total_expense = transactions.filter(type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
+    # total_amt = Budget.objects.first()
+    # print(total_amt)
+    context = get_data(request)
+
+    return render(request, 'index.html', context)
 
 
 
@@ -32,13 +46,16 @@ def dashboard(request):
             
 
 def get_transaction(request):
+
     return render(request, 'index.html') 
 
 def all_transaction(request):
-    transactions = Transaction.objects.all()
-    return render(request, 'index.html', {'is_transc': True, 'transactions': transactions}) 
+    # transactions = Transaction.objects.all()
+    context = get_data(request)
+    context['is_transc'] = True
+    return render(request, 'index.html', context) 
 
-def get_budget(request):
-    total_amt = Budget.objects.filter(total_balance=0).values_list('total_balance', flat=True)
-    print(total_amt)
-    return render(request, 'index.html',{'total_amount':total_amt})
+# def get_budget(request):
+#     total_amt = Budget.objects.filter(total_balance=0).values_list('total_balance', flat=True)
+#     print(total_amt)
+#     return render(request, 'index.html',{'total_amount':total_amt})
